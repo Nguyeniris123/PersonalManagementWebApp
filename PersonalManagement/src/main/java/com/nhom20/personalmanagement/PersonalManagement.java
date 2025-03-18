a8b08a1597c48abba78515dfc51f466fdcbf54d0
Hồ Chí Nguyên <136091628+Nguyeniris123@users.noreply.github.com>
2025-03-18 17:13:34 +0700

    update porm.xml, tạo package

---
+++ PersonalManagement/src/main/java/com/nhom20/personalmanagement/PersonalManagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.nhom20.personalmanagement;
+
+/**
+ *
+ * @author nguyenho
+ */
+public class PersonalManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
