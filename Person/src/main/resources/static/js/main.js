f1cfc9905a7d68b1bbf72702efd9ca917dc62b8c
Hồ Chí Nguyên <136091628+Nguyeniris123@users.noreply.github.com>
2025-04-08 16:33:37 +0700

    Xoa user

--- Person/src/main/resources/static/js/main.js
+++ Person/src/main/resources/static/js/main.js
@@ -12,3 +12,18 @@ function deleteHealthProfile(id) {
         });
     }
 }
+
+function deleteUser(id) {
+    if (confirm("Bạn có chắc chắn muốn xoá không?")) {
+        fetch(`/Person/api/users/${id}`, {
+            method: "DELETE"
+        }).then(res => {
+            if (res.status === 204) {
+                alert("Đã xoá thành công!");
+                location.reload();
+            } else {
+                alert("Xoá thất bại!");
+            }
+        });
+    }
+}
